5e1a5e275d6782bafa452078a219aac5f35407f7
TowardsAurora <90918138+TowardsAurora@users.noreply.github.com>
2022-12-17 15:20:44 +0800

    Update captcha_predict.py

--- captcha_predict.py
+++ captcha_predict.py
@@ -46,6 +46,43 @@ def main():
         vis.images(image, opts=dict(caption=c))
 
 
+def predict():
+    cnn = CNN()
+    cnn.eval()
+    cnn.load_state_dict(torch.load('saved_model/model.pkl'))
+    print("load cnn net.")
+
+    predict_dataloader = my_dataset.get_predict_no_shuffle_data_loader()
+
+    vis = Visdom()
+    for i, (images, labels) in enumerate(predict_dataloader):
+        image = images
+        vimage = Variable(image)
+        predict_label = cnn(vimage)
+
+        listdir = os.listdir('./dataset/predict')
+
+        print("true:", listdir[i].split('_')[0])
+
+        c0 = captcha_setting.ALL_CHAR_SET[
+            np.argmax(predict_label[0, 0:captcha_setting.ALL_CHAR_SET_LEN].data.numpy())]
+        c1 = captcha_setting.ALL_CHAR_SET[
+            np.argmax(
+                predict_label[0, captcha_setting.ALL_CHAR_SET_LEN:2 * captcha_setting.ALL_CHAR_SET_LEN].data.numpy())]
+        c2 = captcha_setting.ALL_CHAR_SET[np.argmax(
+            predict_label[0, 2 * captcha_setting.ALL_CHAR_SET_LEN:3 * captcha_setting.ALL_CHAR_SET_LEN].data.numpy())]
+        c3 = captcha_setting.ALL_CHAR_SET[np.argmax(
+            predict_label[0, 3 * captcha_setting.ALL_CHAR_SET_LEN:4 * captcha_setting.ALL_CHAR_SET_LEN].data.numpy())]
+
+        c = '%s%s%s%s' % (c0, c1, c2, c3)
+        print("predict:", c)
+
+        # vis.images(image, opts=dict(caption=c))
+        vis.images(image, opts=dict(caption=c))        
+        
+        
+        
 if __name__ == '__main__':
     count_img_nums()
     main()
+    # or # predict()
